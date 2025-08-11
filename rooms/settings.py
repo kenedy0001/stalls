59da1b462bedfb3084abd47aee66253a194ac0ef
kenedy0001 <kenedykombo20@gmail.com>
2025-08-11 19:39:41 +0300

    Update settings.py

--- rooms/settings.py
+++ rooms/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']# Default primary key field type
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
